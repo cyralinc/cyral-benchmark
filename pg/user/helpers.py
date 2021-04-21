18225184caa85395b7d96c3c32d645fa035958ad
Ahmed Marzouk <amarzouk@cyral.com>
2021-04-21 22:08:07 +0200

    add more informative title to user load results table

--- pg/user/helpers.py
+++ pg/user/helpers.py
@@ -3,15 +3,15 @@ from config import config
 
 
 def pretty_print_results(average_latencies_per_test):
-    # printing quereis used
+    # printing test queries
     print("\nQueries used:\n")
     for index, query in enumerate(config["queries"]):
         print(f"Query #{index+1}: '{query}'")
-    # preparing table rows
-    print("\nResults:\n")
+    # preparing table 
+    print("\nAverage Latencies:\n")
     titles_row = []
-    for sessions_number in config["concurrent_users"]:
-        titles_row.append(f"{sessions_number} concurrent users")
+    for no_of_users in config["concurrent_users"]:
+        titles_row.append(f"{no_of_users} concurrent users")
     table = PrettyTable([""] + titles_row)
     for key, latencies in average_latencies_per_test.items():
         table.add_row([key] + [f"{round(val, 4):.4f} ms" for val in latencies])
@@ -34,5 +34,6 @@ def parse_pgbench_output(pgbench_output):
 
 def get_millisec(formatted_time):
     """time is assumed to be formatted as hh:mm:ss"""
-    h, m, s = formatted_time.split(":")
+    splitted = formatted_time.split(":")
+    h, m, s =  splitted if len(splitted) is 3 else ([0] + splitted)
     return 1000 * (int(h) * 3600 + int(m) * 60 + int(s))
