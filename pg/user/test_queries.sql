f44202fb578bed414aa08a85410c13a9eab81588
Ahmed Marzouk <amarzouk@cyral.com>
2021-04-21 22:44:26 +0200

    comments out the last select query as it requires large memory

--- pg/user/test_queries.sql
+++ pg/user/test_queries.sql
@@ -1,6 +1,6 @@
 SELECT * FROM pgbench_accounts LIMIT 10
 SELECT * FROM pgbench_accounts LIMIT 100
 SELECT * FROM pgbench_accounts LIMIT 1000
-SELECT * FROM pgbench_accounts LIMIT 10000 
+SELECT * FROM pgbench_accounts LIMIT 10000  
 SELECT * FROM pgbench_accounts LIMIT 100000 
-SELECT * FROM pgbench_accounts LIMIT 1000000 
+-- SELECT * FROM pgbench_accounts LIMIT 1000000 --requires more memory in the system to run this test
